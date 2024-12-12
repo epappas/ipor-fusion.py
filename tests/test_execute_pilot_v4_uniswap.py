0191f47ef17e92132f9e8f6e726162d7dd3ae8a1
amydlarz <amydlarz@gmail.com>
2024-12-12 12:29:09 +0100

    mv

--- tests/test_execute_pilot_v4_uniswap.py
+++ tests/test_execute_pilot_v4_uniswap.py
@@ -102,6 +102,96 @@ def test_should_open_new_position_uniswap_v3():
     assert usdc_change < -400_000000
     assert usdt_change < -400_000000
 
+def test_should_collect_all_after_decrease_liquidity():
+    # Reset state and grant necessary roles
+    # anvil.reset_fork(254084008)
+
+    system = PlasmaVaultSystemFactory(
+        provider_url=anvil.get_anvil_http_url(),
+        private_key=ANVIL_WALLET_PRIVATE_KEY,
+    ).get(ARBITRUM.PILOT.V5.PLASMA_VAULT)
+
+    cheating = CheatingPlasmaVaultSystemFactory(
+        provider_url=anvil.get_anvil_http_url(),
+        private_key=ANVIL_WALLET_PRIVATE_KEY,
+    ).get(ARBITRUM.PILOT.V5.PLASMA_VAULT)
+
+    cheating.prank(system.access_manager().owner())
+    cheating.access_manager().grant_role(Roles.ALPHA_ROLE, ANVIL_WALLET, 0)
+
+    # Swap USDC to USDT
+    swap = system.uniswap_v3().swap(
+        token_in_address=system.usdc().address(),
+        token_out_address=system.usdt().address(),
+        fee=100,
+        token_in_amount=int(500e6),
+        min_out_amount=0,
+    )
+    system.plasma_vault().execute([swap])
+
+    # Create a new position with specified parameters
+    new_position = system.uniswap_v3().new_position(
+        token0=system.usdc().address(),
+        token1=system.usdt().address(),
+        fee=100,
+        tick_lower=-100,
+        tick_upper=101,
+        amount0_desired=int(499e6),
+        amount1_desired=int(499e6),
+        amount0_min=0,
+        amount1_min=0,
+        deadline=int(time.time()) + 100,
+    )
+    tx = system.plasma_vault().execute([new_position])
+
+    # Extract data from the new position creation event
+    _, new_token_id, liquidity, *_ = extract_enter_data_form_new_position_event(tx)
+
+    # Decrease the liquidity of the newly created position
+    decrease_action = system.uniswap_v3().decrease_position(
+        token_id=new_token_id,
+        liquidity=liquidity,
+        amount0_min=0,
+        amount1_min=0,
+        deadline=int(time.time()) + 100,
+    )
+    system.plasma_vault().execute([decrease_action])
+
+    # Check balances before the collect action
+    vault_usdc_balance_before = system.usdc().balance_of(ARBITRUM.PILOT.V5.PLASMA_VAULT)
+    vault_usdt_balance_before = system.usdt().balance_of(ARBITRUM.PILOT.V5.PLASMA_VAULT)
+
+    # Perform the collect action
+    collect = system.uniswap_v3().collect(token_ids=[new_token_id])
+    system.plasma_vault().execute([collect])
+
+    # Check balances after the collect action
+    vault_usdc_balance_after = system.usdc().balance_of(ARBITRUM.PILOT.V5.PLASMA_VAULT)
+    vault_usdt_balance_after = system.usdt().balance_of(ARBITRUM.PILOT.V5.PLASMA_VAULT)
+
+    collect_usdc_change = vault_usdc_balance_after - vault_usdc_balance_before
+    collect_usdt_change = vault_usdt_balance_after - vault_usdt_balance_before
+
+    # Assert on balance changes after collect action
+    assert (
+        498_000000 < collect_usdc_change < 500_000000
+    ), "USDC balance after collect is out of expected range"
+    assert (
+        489_000000 < collect_usdt_change < 500_000000
+    ), "USDT balance after collect is out of expected range"
+
+    # Close the position
+    close_position = system.uniswap_v3().close_position(token_ids=[new_token_id])
+    receipt = system.plasma_vault().execute([close_position])
+
+    # Extract data from the position closing event
+    _, close_token_id = extract_exit_data_form_new_position_event(receipt)
+
+    # Assert that the token ID of the new position matches the closed position's token ID
+    assert (
+        new_token_id == close_token_id
+    ), "Token ID of new position does not match closed position"
+
 
 def test_should_increase_liquidity():
     # Setup: Reset state and grant necessary roles
